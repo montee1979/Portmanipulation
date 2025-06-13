8b4b283e91bb0f0c6b19da8986c4bf66834ac7a5
montee1979 <109105788+montee1979@users.noreply.github.com>
2025-06-13 16:45:45 -0400

    Add files via upload

---
+++ Portmanipulation-main/Portmanipulation.cpp
@@ -0,0 +1,52 @@
+#include "Portmanipulation.h"
+
+void pins::pinset(int pin, int state) {
+  if (pin >= 0 && pin <= 7) {
+    if (state == 1) {
+      PORTD |= (1 << pin);
+    } else if (state == 0) {
+      PORTD &= ~(1 << pin);
+    }
+  } else if (pin >= 8 && pin <= 13) {
+    pin = pin - 8;
+    if (state == 1) {
+      PORTB |= (1 << pin);
+    } else if (state == 0) {
+      PORTB &= ~(1 << pin);
+    }
+  }
+}
+
+void pins::pinmode(int pin, int state) {
+  if (pin >= 0 && pin <= 7) {
+    if (state == 1) {
+      DDRD |= (1 << pin);
+    } else if (state == 0) {
+      DDRD &= ~(1 << pin);
+    }
+  } else if (pin >= 8 && pin <= 13) {
+    if (state == 1) {
+      DDRB |= (1 << (pin - 8));
+    } else if (state == 0) {
+      DDRB &= ~(1 << (pin - 8));
+    }
+  }
+}
+
+pins Pins;
+
+void pause::microsecs(unsigned long delayTime) {
+  unsigned long startTime = micros();
+  while (micros() - startTime < delayTime) {
+    //wait
+  }
+}
+
+void pause::millisecs(unsigned long delayTime){
+  delayTime = delayTime * 1000;
+  unsigned long startTime = micros();
+  while (micros() - startTime < delayTime) {
+    //wait
+  }
+}
+pause Pause;
---
+++ Portmanipulation-main/Portmanipulation.h
@@ -0,0 +1,22 @@
+#ifndef PORTMANIPULATION_H
+#define PORTMANIPULATION_H
+
+#include <Arduino.h>
+
+class pins {
+	public:
+		void pinset(int pin, int state);
+		void pinmode(int pin, int state);
+};
+
+extern pins Pins;
+
+class pause {
+	public:
+		void microsecs(unsigned long delayTime);
+		void millisecs(unsigned long delayTime);
+};
+
+extern pause Pause;
+
+#endif
